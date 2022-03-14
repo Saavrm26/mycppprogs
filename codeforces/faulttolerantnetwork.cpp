35cafb0aa546c16f226abbd3dc326f8656b69174
Saarthak Verma <saarthakverma739@gmail.com>
2022-03-14 20:55:29 +0530

    faultnetwork

--- codeforces/faulttolerantnetwork.cpp
+++ codeforces/faulttolerantnetwork.cpp
@@ -3,16 +3,10 @@ using namespace std;
 
 #define vi vector<int>
 #define ll long long
-#define vll vector<long long>
-int row_mini(vector<int> vec,int x){
-    int row_mini=INT32_MAX;
-    int n=vec.size();
-    for(int i=0;i<n;i++){
-        if(row_mini>abs(vec[i]-x)){
-            row_mini=abs(vec[i]-x);
-        }
-    }
-    return row_mini;
+ll mini(ll case_min,ll ans){
+    if(case_min<ans)
+        ans=case_min;
+    return ans;
 }
 int main(){
     ios_base::sync_with_stdio(false);
@@ -23,39 +17,107 @@ int main(){
     while(t--){
         int n;
         cin>>n;
-        vector<pair<int,int>> a(n);
-        vector<pair<int,int>> b(n);
+        vi a(n);
+        vi b(n);
         for(int i=0;i<n;i++){
-            int q;
-            cin>>q;
-            if(i==0||i==n-1)
-                a[i]=make_pair(q,1);
-            else
-                a[i]={q,0};
+            cin>>a[i];
         }
-        vector<pair<int,int>> A;
-        A.push_back(a[0]);A.push_back(a[n-1]);       
         for(int i=0;i<n;i++){
-            int q;
-            cin>>q;
-            if(i==0||i==n-1)
-                b[i]={q,1};
-            else
-                b[i]={q,0};
+            cin>>b[i];
         }
-        vector<pair<int,int> B;
-        B.push_back(b[0]);B.push_back(b[n-1]);
-        vector<pair<int,int> minis;
-        minis.push_back(row_mini(b,A[0]));
-        minis.push_back(row_mini(b,A[1]));
-        minis.push_back(row_mini(a,B[0]));
-        minis.push_back(row_mini(a,B[1]));
-        sort(minis.begin(),minis.end());
-        if(==(minis[0]+minis[1]))
-            cout<<(ll)(minis[0])+(ll)(minis[1])<<"\n";
-        else{
-            cout<<(ll)(minis[0])+(ll)(minis[1])+(ll)(minis[2])+(ll)(minis[3])<<"\n";
+        
+        ll ans=INT32_MAX;
+        //case 1
+        ll case_min=(ll)(abs(a[0]-b[0]))+(ll)(abs(a[n-1]-b[n-1]));
+        ans=mini(case_min,ans);
+        //case 2
+        case_min=(ll)(abs(a[0]-b[n-1]))+(ll)(abs(a[n-1]-b[0]));
+        ans=mini(case_min,ans);
+        //case 3
+        case_min=(ll)(abs(a[n-1]-b[n-1]));
+        ll a_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //a0  - bx
+            if((ll)(abs(a[0]-b[i]))<a_min)
+                a_min=(ll)(abs(a[0]-b[i]));
+        }      
+        ll b_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //b0  - ax
+            if((ll)(abs(b[0]-a[i]))<b_min)
+                b_min=(ll)(abs(b[0]-a[i]));
         }
-        cout<<min(((ll)(minis[0])+(ll)(minis[1])+(ll)(minis[2])+(ll)(minis[3])),min(((ll)(abs(a[0]-b[0]))+(ll)abs(a[n-1]-b[n-1])),((ll)(abs((ll)a[0]-(ll)b[n-1]))+(ll)abs((ll)a[n-1]-(ll)b[0]))))<<"\n";
+        case_min += a_min + b_min;
+        ans=mini(case_min,ans);
+
+        //case 4
+        case_min=(ll)(abs(a[0]-b[0]));
+        a_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //an-1  - bx
+            if((ll)(abs(a[n-1]-b[i]))<a_min)
+                a_min=(ll)(abs(a[n-1]-b[i]));
+        }      
+        b_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //bn-1  - ax
+            if((ll)(abs(b[n-1]-a[i]))<b_min)
+                b_min=(ll)(abs(b[n-1]-a[i]));
+        }
+        case_min += a_min + b_min;
+        ans=mini(case_min,ans);
+
+        //case 5
+        case_min=(ll)(abs(b[0]-a[n-1]));
+        a_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //a0  - bx
+            if((ll)(abs(a[0]-b[i]))<a_min)
+                a_min=(ll)(abs(a[0]-b[i]));
+        }      
+        b_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //bn-1  - ax
+            if((ll)(abs(b[n-1]-a[i]))<b_min)
+                b_min=(ll)(abs(b[n-1]-a[i]));
+        }
+        case_min += a_min + b_min;
+        ans=mini(case_min,ans);
+        
+        //case 6
+
+        case_min=(ll)(abs(b[n-1]-a[0]));
+        a_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //an-1  - bx
+            if((ll)(abs(a[n-1]-b[i]))<a_min)
+                a_min=(ll)(abs(a[n-1]-b[i]));
+        }      
+        b_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //b0  - ax
+            if((ll)(abs(b[0]-a[i]))<b_min)
+                b_min=(ll)(abs(b[0]-a[i]));
+        }
+        case_min += a_min + b_min;
+        ans=mini(case_min,ans);
+        
+        // case 7
+        a_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //a0  - bx
+            if((ll)(abs(a[0]-b[i]))<a_min)
+                a_min=(ll)(abs(a[0]-b[i]));
+        }      
+        b_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //bn-1  - ax
+            if((ll)(abs(b[n-1]-a[i]))<b_min)
+                b_min=(ll)(abs(b[n-1]-a[i]));
+        }
+        case_min=a_min+b_min;
+        a_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //an-1  - bx
+            if((ll)(abs(a[n-1]-b[i]))<a_min)
+                a_min=(ll)(abs(a[n-1]-b[i]));
+        }      
+        b_min=INT32_MAX;
+        for(int i=1;i<n-1;i++){                //b0  - ax
+            if((ll)(abs(b[0]-a[i]))<b_min)
+                b_min=(ll)(abs(b[0]-a[i]));
+        }
+        case_min += a_min + b_min;
+        ans=mini(case_min,ans);
+        cout<<ans<<"\n";
     }
 }
