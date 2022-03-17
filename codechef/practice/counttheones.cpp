c4fa6c1ab410b2bc5c9e4f8fdf626d56473ba555
Saarthak Verma <saarthakverma739@gmail.com>
2022-03-17 15:12:49 +0530

    another try

--- codechef/practice/counttheones.cpp
+++ codechef/practice/counttheones.cpp
@@ -4,14 +4,13 @@ using namespace std;
 #define vi vector<int>
 #define ll long long
 #define vll vector<long long>
-vll fact_lookup(51);
-ll ncr(ll n,ll r){
-    return (fact_lookup[n]/(fact_lookup[r]*fact_lookup[n-r]));
-}
+
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    vll fact_lookup(51);
     fact_lookup[0]=1;
     fact_lookup[1]=1;
     for(ll i=2;i<=50;i++){
@@ -23,12 +22,12 @@ int main(){
         ll n,k;
         cin>>n>>k;
         ll r=1,ans=0,sum=0;
-        ll temp=ncr(n,r);
-        while(sum+temp<=k){
+        ll temp=(fact_lookup[n]/(fact_lookup[r]*fact_lookup[n-r]));
+        while(sum+temp<k){
             sum += temp;
             ans += temp*r;
             r++; 
-            temp=ncr(n,r);
+            temp=(fact_lookup[n]/(fact_lookup[r]*fact_lookup[n-r]));
         }
         ans += (k-sum)*r;
         cout<<ans<<"\n";
