05b3e1df14a3550fe74396a3628c9815fc06f3bb
saarthak <lci2021035@iiitl.ac.in>
2022-06-12 13:31:28 +0530

    backup again

---
+++ gfg/mcm.cpp
@@ -0,0 +1,121 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+//datatype snippets
+typedef long long ll;
+//stl snippets
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<pair<int,int>> vpii;
+typedef vector<pair<char,int>> vpci;
+typedef vector<pair<long long,long long>> vpll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef pair<int,int> pii;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int,int> mii;
+typedef map<long long,long long> mll;
+typedef map<int,pair<int,int>> mipii;
+#define eb emplace_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+//io snippets
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ini(x) int x;cin>>x;
+#define inll(x) ll x;cin>>x;
+#define ins(x) string x;cin>>x;
+#define invi(x,n) vi x(n);ff(i,0,n-1) cin>>x[i];
+#define invll(x,n) vll x(n);ff(i,0,n-1) cin>>x[i];
+// loop snippets
+#define ff(i,init,fin) for(int i=init;i<=fin;i++)
+#define fb(i,init,fin) for(int i=init;i>=fin;i--)
+#define ffs(i,init,fin,step) for(int i=init;i<=fin;i=i+step)
+#define fbs(i,init,fin,step) for(int i=init;i>=fin;i=i-step)
+#define ffit(it,x) for(auto it=x.begin();it!=x.end();it++)
+#define ffa(it,x) for(auto it:x)
+//bit snippets
+#define popcnt __builtin_popcount
+//function snippets
+ll minimum(ll a,ll b){if(a<b) return a;else return b;}
+ll maximum(ll a,ll b){if(a>b) return a;else return b;}
+ll absolute(ll a){if(a>=0)return a;else return a*-1;}
+ll lcm (ll a, ll b) {return a / gcd(a, b) * b;}
+ll mod_sub(ll a,ll b){ll mod=1e9+7;return ((a-b)%mod + mod) % mod;}
+ll binpow(ll a, ll b) {ll res = 1;while (b > 0) {if (b & 1) res = res * a;a = a * a;b >>= 1;}return res;}
+//tracing
+#define trace1d(arr,n) cout<<#arr<<"\n";for(int i=0;i<=n;i++)cout<<(arr[i])<<" ";cout<<"\n";
+#define trace2d(arr,n,m) cout<<#arr<<"\n";for(int i=0;i<=n;i++){for(int j=0;j<=m;j++){cout<<(arr[i][j])<<" ";}cout<<"\n";}
+#define trace(x) cout<<#x<<" "<<x<<"\n";
+
+// Questions to ask before submitting any code on OJ
+// Q1. Is my approach handling all the cases ? Think of some edge cases
+// Q2. How complicated is my approach
+// Q3. Will your implementation be a barrier?
+// Remember:
+// Competition is with yourself
+
+class Solution{
+public:
+    int dp[1001][1001];
+    int mcm(int arr[],int i,int j){
+        if(i>=j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ops=INT32_MAX;
+        ff(k,i,j-1){
+            int x1=mcm(arr,i,k);
+            int x2=mcm(arr,k+1,j);
+            int x3=(arr[i-1]*arr[k]*arr[j]);
+            int x=mcm(arr,i,k) + mcm(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
+
+
+            // cout<<"X1 IS "<<x1<<"\n";
+            // cout<<"X2 IS "<<x2<<"\n";
+            // cout<<"X3 IS "<<x3<<"\n";
+            // cout<<"X IS "<<x<<"\n";
+            ops = min(ops,x);
+        }
+        return dp[i][j]=ops;
+    }
+    int matrixMultiplication(int N, int arr[])
+    {
+        memset(dp,-1,sizeof(dp));
+        int y= mcm(arr,1,N-1);
+        // trace2d(dp,1000,1000);
+        return y;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    #endif
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
---
+++ gfg/mcmbUp.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//datatype snippets
+typedef long long ll;
+//stl snippets
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<pair<int,int>> vpii;
+typedef vector<pair<char,int>> vpci;
+typedef vector<pair<long long,long long>> vpll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef pair<int,int> pii;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int,int> mii;
+typedef map<long long,long long> mll;
+typedef map<int,pair<int,int>> mipii;
+#define eb emplace_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define fi first
+#define se second
+//io snippets
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ini(x) int x;cin>>x;
+#define inll(x) ll x;cin>>x;
+#define ins(x) string x;cin>>x;
+#define invi(x,n) vi x(n);ff(i,0,n-1) cin>>x[i];
+#define invll(x,n) vll x(n);ff(i,0,n-1) cin>>x[i];
+// loop snippets
+#define ff(i,init,fin) for(int i=init;i<=fin;i++)
+#define fb(i,init,fin) for(int i=init;i>=fin;i--)
+#define ffs(i,init,fin,step) for(int i=init;i<=fin;i=i+step)
+#define fbs(i,init,fin,step) for(int i=init;i>=fin;i=i-step)
+#define ffit(it,x) for(auto it=x.begin();it!=x.end();it++)
+#define ffa(it,x) for(auto it:x)
+//bit snippets
+#define popcnt __builtin_popcount
+//function snippets
+ll minimum(ll a,ll b){if(a<b) return a;else return b;}
+ll maximum(ll a,ll b){if(a>b) return a;else return b;}
+ll absolute(ll a){if(a>=0)return a;else return a*-1;}
+ll lcm (ll a, ll b) {return a / gcd(a, b) * b;}
+ll mod_sub(ll a,ll b){ll mod=1e9+7;return ((a-b)%mod + mod) % mod;}
+ll binpow(ll a, ll b) {ll res = 1;while (b > 0) {if (b & 1) res = res * a;a = a * a;b >>= 1;}return res;}
+#define trace1d(arr,n) cout<<#arr<<"\n";for(int i=0;i<=n;i++)cout<<(arr[i])<<" ";cout<<"\n";
+#define trace2d(arr,n,m) cout<<#arr<<"\n";for(int i=0;i<=n;i++){for(int j=0;j<=m;j++){cout<<(arr[i][j])<<" ";}cout<<"\n";}
+#define trace(x) cout<<#x<<" "<<x<<"\n";
+
+
+// Questions to ask before submitting any code on OJ
+// Q1. Is my approach handling all the cases ? Think of some edge cases
+// Q2. How complicated is my approach
+// Q3. Will your implementation be a barrier?
+// Remember:
+// Competition is with yourself
+int MatrixChainOrder(int p[], int n)
+{
+
+    /* For simplicity of the program, one
+    extra row and one extra column are
+    allocated in m[][]. 0th row and 0th
+    column of m[][] are not used */
+    int m[n][n];
+    memset(m,0,sizeof(m));
+    int i, j, k, L, q;
+
+    /* m[i, j] = Minimum number of scalar
+    multiplications needed to compute the
+    matrix A[i]A[i+1]...A[j] = A[i..j] where
+    dimension of A[i] is p[i-1] x p[i] */
+
+    // cost is zero when multiplying
+    // one matrix.
+    for (i = 1; i < n; i++)
+        m[i][i] = 0;
+
+    // L is chain length.
+    for (L = 2; L < n; L++)
+    {
+        for (i = 1; i < n - L + 1; i++)
+        {
+            j = i + L - 1;
+            m[i][j] = INT_MAX;
+            for (k = i; k <= j - 1; k++)
+            {
+                // q = cost/scalar multiplications
+                q = m[i][k] + m[k + 1][j]+ p[i - 1] * p[k] * p[j];
+                if (q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+    }
+    trace2d(m, n-1, n-1);
+    return m[1][n - 1];
+}
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    #endif
+    fastIO;
+    int arr[] = {1,2,3,4,5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << "Minimum number of multiplications is "<< MatrixChainOrder(arr, size);
+    getchar();
+    return 0;
+}
